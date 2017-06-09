5ea389bd8cb9e5ba0459e6dab62f27533914b97b
msav <msav@msav.ru>
2017-06-09 16:05:25 +0300

    Changed Parent theme font on a new font with the cyrillic support

--- inc/functions.php
+++ inc/functions.php
@@ -21,3 +21,31 @@ if ( !function_exists('onepressblog_checkbox_sanitize') ) {
 		// onepressblog_checkbox_sanitize
 	}
 }
+
+
+if ( ! function_exists( 'twentysixteen_fonts_url' ) ) {
+	/**
+	 * Register Google fonts for Twenty Sixteen.
+	 *
+	 * @since Twenty Sixteen 1.0
+	 *
+	 * @return string Google fonts URL for the theme.
+	 */
+	function twentysixteen_fonts_url() {
+
+		$fonts_url = '';
+		$fonts     = array();
+		$subsets   = 'cyrillic,cyrillic-ext,latin-ext';
+
+		$fonts[] = 'Open Sans:300,400,400i,600,700';
+
+		if ( $fonts ) {
+			$fonts_url = add_query_arg( array(
+				'family' => urlencode( implode( '|', $fonts ) ),
+				'subset' => urlencode( $subsets ),
+			), 'https://fonts.googleapis.com/css' );
+		}
+
+		return $fonts_url;
+	}
+}
--- inc/template-functions.php
+++ inc/template-functions.php
@@ -4,8 +4,8 @@ if ( !function_exists('onepressblog_enqueue_scripts') ) {
 	function onepressblog_enqueue_scripts() {
 
 		// Include Parent stylesheets
-		$parent_style = 'twentysixteen-style';
-		wp_enqueue_style( $parent_style, _OPB_PARENT_URI . '/style.css' );
+		//$parent_style = 'twentysixteen-style';
+		//wp_enqueue_style( $parent_style, _OPB_PARENT_URI . '/style.css' );
 
 		// Include current theme main stylesheet
 		$onepressblog_style = 'onepressblog_style';
